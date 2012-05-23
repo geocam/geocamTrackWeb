4662f73e2826d221f647d7757b0ef0533c2c62db
Trey Smith <trey.smith@nasa.gov>
2012-05-23 13:48:51 +0000

    added filter.py

---
+++ geocamTrack/filter.py
@@ -0,0 +1,28 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from geocamTrack.models import getModelByName
+from geocamTrack import settings
+
+PAST_POSITION_MODEL = getModelByName(settings.GEOCAM_TRACK_PAST_POSITION_MODEL)
+
+class PositionFilter(object):
+    def __init__(self, distanceMeters, pointAddedHandler=None):
+        self.distanceMeters = distanceMeters
+        self.pointAddedHandler = pointAddedHandler
+
+        pastPositions = PAST_POSITION_MODEL.objects.all().order_by('-timestamp')
+        if pastPositions.count():
+            self.previousPos = pastPositions[0]
+        else:
+            self.previousPos = None
+
+    def add(self, pos):
+        if self.previousPos is not None and pos.getDistance(self.previousPos) > self.distanceMeters:
+            pos.save()
+            return True
+        else:
+            return False
