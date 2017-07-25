a1ca4f19a5079b3925fe06e61e89973f45e75285
Tamar Cohen <tamar.cohen@nasa.gov>
2017-07-25 16:29:05 -0700

    changed the way we format times when we search for position

--- geocamTrack/utils.py
+++ geocamTrack/utils.py
@@ -58,8 +58,9 @@ def getClosestPosition(track=None, timestamp=None, max_time_difference_seconds=s
         # limit time to range
         mintime = timestamp - datetime.timedelta(seconds=max_time_difference_seconds)
         maxtime = timestamp + datetime.timedelta(seconds=max_time_difference_seconds)
-        query = query + operand + " pos.timestamp >= '" + mintime.isoformat() + "' and pos.timestamp <= '" + maxtime.isoformat() + "'"
-        query = query + " order by abs(timestampdiff(second, '" + timestamp.isoformat() + "', timestamp)) limit 1;"
+        query = query + operand + " pos.timestamp >= '" + mintime.strftime('%Y-%m-%d %H:%M:%S') + "' and pos.timestamp <= '" + maxtime.strftime('%Y-%m-%d %H:%M:%S') + "'"
+        query = query + " order by abs(timestampdiff(second, '" + timestamp.strftime('%Y-%m-%d %H:%M:%S') + "', timestamp)) limit 1;"
+        print query
         posAtTime = (PAST_POSITION_MODEL.get().objects.raw(query))
         posList = list(posAtTime)
         if posList:
